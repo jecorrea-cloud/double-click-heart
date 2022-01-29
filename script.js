ce503d69664c78a625f9f8b031f99a5b3283ccdd
joaquin <jecorrea@protonmail.com>
2022-01-29 13:22:09 -0500

    full functionality

--- script.js
+++ script.js
@@ -4,16 +4,18 @@ const loveMe = document.querySelector(".loveMe");
 const times = document.querySelector("#times");
 
 let clickTime = 0;
+let timesClicked = 0;
 
 loveMe.addEventListener("click", (e) => {
   if (clickTime === 0) {
     clickTime = new Date().getTime();
-    console.log(clickTime);
+    // console.log(clickTime);
   } else {
     if (new Date().getTime() - clickTime < 800) {
+      // function for double clicking defined on line 24
       createHeart(e);
       clickTime = 0;
-      console.log(123);
+      //   console.log(123);
     } else {
       clickTime = new Date().getTime();
     }
@@ -24,4 +26,28 @@ const createHeart = (e) => {
   const heart = document.createElement("i");
   heart.classList.add("fas");
   heart.classList.add("fa-heart");
+
+  const x = e.clientX;
+  const y = e.clientY;
+
+  //   console.log(x, y);
+
+  // To make values relative to the image
+
+  const leftOffset = e.target.offsetLeft;
+  const topOffset = e.target.offsetTop;
+
+  const xInside = x - leftOffset;
+  const yInside = y - topOffset;
+
+  //   console.log(xInside, yInside);
+
+  heart.style.top = `${yInside}px`;
+  heart.style.left = `${xInside}px`;
+
+  loveMe.appendChild(heart);
+
+  times.innerHTML = ++timesClicked;
+
+  setTimeout(() => heart.remove(), 1000);
 };
